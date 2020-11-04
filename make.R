d072e1df6be7e96a1aea2a0baaabf4d5d9733886
Diego AYALA <diego.ayala@ird.fr>
2020-11-04 09:08:35 +0100

    :dog: Changes in make.R

---
+++ make.R
@@ -0,0 +1,21 @@
+############
+#
+# Data and Code for the CESAB Datatoolbox Exercises
+#
+##################################
+
+
+#!/usr/bin/env Rscript
+
+# ----- clean workspace
+rm(list = ls())
+
+# ----- install/update packages
+devtools::install_deps()
+
+# ----- install compendium package
+devtools::install(build = FALSE)
+
+# ----- Knit exo dplyr
+rmarkdown::render(here::here("exercices","exo_dplyr.Rmd"))
+
