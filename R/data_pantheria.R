7c24ccb61c13f94dabfa097151f29ce911a0139b
Diego AYALA <diego.ayala@ird.fr>
2020-11-04 13:06:10 +0100

    :tiger: exo ggplot

--- R/data_pantheria.R
+++ R/data_pantheria.R
@@ -6,8 +6,6 @@
 #'
 data_pantheria <- function() {
 
-  readr::read_csv(here::here("data", "pantheria-traits", "PanTHERIA_1-0_WR05_Aug2008.txt"))
+  readr::read_delim(here::here("data", "pantheria-traits", "PanTHERIA_1-0_WR05_Aug2008.txt"),  delim = "\t")
 
 }
-
-
